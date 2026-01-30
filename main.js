359193a7bcd6afb0242eb7230f7d85290b031f80
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-01-30 18:12:11 -0500

    added boilerplate to index.html and set up main.js to serve it

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, ipcMain, Menu } = require("electron/main");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    // fullscreen: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+
+  win.loadFile("index.html");
+  win.maximize();
+
+  if (!app.isPackaged) {
+    win.webContents.openDevTools();
+  }
+};
+
+app.whenReady().then(() => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit;
+  }
+});
