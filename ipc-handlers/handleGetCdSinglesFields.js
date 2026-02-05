d5fe174a9cc2951ad44028e5787bcdbefcd4d683
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-02-05 14:52:13 -0500

    refactored handleGetCdSinglesFields, abstracted the complex query to its own module

--- ipc-handlers/handleGetCdSinglesFields.js
+++ ipc-handlers/handleGetCdSinglesFields.js
@@ -1,4 +1,5 @@
 const pool = require("../dbconnect.js");
+const cdSinglesQuery = require("./queries/cdSinglesQuery");
 
 /**
  * query and return the fields of the cd singles and cd singles tracks table
@@ -8,14 +9,8 @@ const pool = require("../dbconnect.js");
 async function handleGetCdSinglesFields(e) {
   const fieldList = [];
 
-  const query = `
-    SELECT column_name FROM information_schema.columns WHERE table_name = 'cd_singles'
-    UNION
-    SELECT column_name FROM information_schema.columns WHERE table_name = 'cd_singles_tracks';
-    `;
-
   try {
-    const result = await pool.query(query);
+    const result = await pool.query(cdSinglesQuery);
 
     result.rows.forEach((row) => {
       fieldList.push(row.column_name);
