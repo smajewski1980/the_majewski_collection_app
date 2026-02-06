adca21b81ab0dfed292b4c28d90acae3e4b1eaa5
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-02-05 20:35:46 -0500

    made the term input active after a search field is selected

--- renderer.js
+++ renderer.js
@@ -3,13 +3,19 @@ const format = document.getElementById("query-format");
 const field = document.getElementById("query-field");
 const term = document.getElementById("query-term");
 const btnLookup = document.getElementById("btn-lookup");
-const getFormatFields = window.getFormatFields;
 
 ut.toggleInertEl(field, true);
 ut.toggleInertEl(term, true);
 
+// when the format is selected, the field select is made active
+// and has its options loaded depending on which format
 format.addEventListener("change", (e) => {
   ut.handleFormatSelection(e, field);
+});
+
+// when the field to search is selected, make term input active
+field.addEventListener("change", (e) => {
+  ut.toggleInertEl(term, false);
 });
 
 // once this is "hooked up", move to utils
