386e747ec3bb95f41a20262715282ed8df1a5504
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-02-08 17:14:57 -0500

    abstracted getHeader() and setArtistColWidths() from displayCdsMain for reuseability in records and tapes

--- utils.js
+++ utils.js
@@ -120,34 +120,13 @@ const utils = {
       utils.displayNotFound(termEl);
       return;
     }
-    // funcs to create the needed elements
-    const makeSpan = () => document.createElement("span");
-    const makeP = () => document.createElement("p");
-    // create a header for the result data
-    const p = makeP();
-    p.className = "result-header";
 
-    const span1 = makeSpan();
-    span1.className = "span1";
-    span1.textContent = "ID";
-    const span2 = makeSpan();
-    span2.className = "span2";
-    span2.textContent = "ARTIST";
-    const span3 = makeSpan();
-    span3.className = "span3";
-    span3.textContent = "TITLE";
-    const span4 = makeSpan();
-    span4.className = "span4";
-    span4.textContent = "LOCATION";
-
-    p.append(span1, span2, span3, span4);
-
-    utils.resultsElement.append(p);
+    utils.resultsElement.append(utils.getHeader());
     // loop through the results, make and append elements to display the data
     rows.forEach((row) => {
-      const p = makeP();
+      const p = utils.makeP();
       Object.values(row).forEach((val, idx) => {
-        const span = makeSpan();
+        const span = utils.makeSpan();
         span.className = `span${idx + 1}`;
         span.textContent = val;
         span.title = val;
@@ -156,23 +135,7 @@ const utils = {
       utils.resultsElement.append(p);
     });
 
-    // for styling, the title field fills the available space, this finds
-    // the max width of values in the artist column and sets the width of all
-    // values in that column to that width, everything is centered nicely
-
-    // get the spans and create an empty set
-    const secondSpans = document.querySelectorAll("#query-results .span2");
-    const secondSpanWidths = new Set();
-    // get the width vals into the set
-    secondSpans.forEach((span) => {
-      secondSpanWidths.add(span.getBoundingClientRect().width);
-    });
-    // set the header and col vals to the max width
-    const maxWidth = Math.max(...secondSpanWidths);
-    span2.style.width = `${maxWidth > 650 ? 650 : maxWidth}px`;
-    secondSpans.forEach((span) => {
-      span.style.width = `${maxWidth > 650 ? 650 : maxWidth}px`;
-    });
+    utils.setArtistColWidths();
   },
   displayRecords: (rows, termEl) => {
     // if no results, show msg
@@ -203,8 +166,57 @@ const utils = {
       });
     }, 3500);
   },
+  /**
+   * constructs and return the p element that is the results header
+   * @returns {HTMLParagraphElement}
+   */
+  getHeader: () => {
+    const p = utils.makeP();
+    p.className = "result-header";
+
+    const span1 = utils.makeSpan();
+    span1.className = "span1";
+    span1.textContent = "ID";
+    const span2 = utils.makeSpan();
+    span2.className = "span2";
+    span2.textContent = "ARTIST";
+    const span3 = utils.makeSpan();
+    span3.className = "span3";
+    span3.textContent = "TITLE";
+    const span4 = utils.makeSpan();
+    span4.className = "span4";
+    span4.textContent = "LOCATION";
+
+    p.append(span1, span2, span3, span4);
+
+    return p;
+  },
+  /**
+   * for styling, the title field fills the available space, this finds
+   * the max width of values in the artist column and sets the width of all
+   * values in that column to that width or a max of 600px,
+   * everything is centered nicely
+   */
+  setArtistColWidths: () => {
+    // get the spans and create an empty set
+    const secondSpans = document.querySelectorAll("#query-results .span2");
+    const secondSpanWidths = new Set();
+    // get the width vals into the set
+    secondSpans.forEach((span) => {
+      secondSpanWidths.add(span.getBoundingClientRect().width);
+    });
+    // set the header and col vals to the max width
+    const maxWidth = Math.max(...secondSpanWidths);
+    document.querySelector(".result-header .span2").style.width =
+      `${maxWidth > 600 ? 600 : maxWidth}px`;
+    secondSpans.forEach((span) => {
+      span.style.width = `${maxWidth > 600 ? 600 : maxWidth}px`;
+    });
+  },
   resultsElement: document.getElementById("query-results"),
   messageDiv: document.getElementById("message"),
+  makeSpan: () => document.createElement("span"),
+  makeP: () => document.createElement("p"),
 };
 
 export default utils;
