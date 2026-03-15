b91010472ab73339c2786f304827167965a6db71
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-03-15 17:31:02 -0400

    moved over the code for the add page from the express app, commented it all to be applied as needed. got the light/dark theme selector working

---
+++ add-script.js
@@ -0,0 +1,581 @@
+import {
+  //   removeActiveFormClass,
+  //   yearFormatIsGood,
+  //   noEmptyFields,
+  //   toasty,
+  //   trimTracks,
+  //   handleIncrementReset,
+  //   incrementCheckbox,
+  //   handleCheckbox,
+  //   incrementLocationSwitch,
+  //   trimDataFields,
+  //   addToSessionList,
+  //   focusFirstField,
+  handleThemeChange,
+  //   removeActiveClass,
+  //   showForm,
+  //   initialShowForm,
+  //   isLocValValid,
+} from "./add-utils.js";
+// import { getLocations } from "./getCurrentLocations.js";
+const cdCompsForm = document.getElementById("cd-comps-form");
+const cdSinglesForm = document.getElementById("cd-singles-form");
+const cdsMainForm = document.getElementById("cd-main-form");
+const recordsForm = document.getElementById("records-form");
+const tapesForm = document.getElementById("tapes-form");
+const btnComps = document.querySelector(".btn-cd-comps");
+const btnSingles = document.querySelector(".btn-cd-singles");
+const btnMain = document.querySelector(".btn-cd-main");
+const btnRecords = document.querySelector(".btn-records");
+const btnTapes = document.querySelector(".btn-tapes");
+const navButtons = [btnComps, btnSingles, btnMain, btnRecords, btnTapes];
+const forms = [cdCompsForm, cdSinglesForm, cdsMainForm, recordsForm, tapesForm];
+const incrementWrapper = document.querySelector(".increment-wrapper");
+const sessionListWrapper = document.querySelector(".session-list-wrapper");
+const sessionList = document.getElementById("session-list");
+let showSessionList = false;
+let initialLoad = true;
+const mainEl = document.querySelector("main");
+
+// when a nav button is clicked, show the appropriate form
+function handleNavBtnClick(e) {
+  if (e.target.classList.contains("active-nav-btn")) return;
+
+  if (!initialLoad) {
+    document.startViewTransition(() => {
+      removeActiveFormClass(forms);
+      removeActiveClass(navButtons);
+      // the first arg is the id of the form to show, second arg is the nav btn
+      showForm(e.target.dataset.form, e.target);
+    });
+  } else {
+    showForm(e.target.dataset.form, e.target);
+    // on the initial load, display the increment location option and the main element
+    initialShowForm(mainEl, incrementWrapper);
+    initialLoad = false;
+  }
+}
+
+// async function handleCdCompsForm(e) {
+//   e.preventDefault();
+//   const formData = new FormData(cdCompsForm);
+
+// get the option elems
+// const cdCompOptionElems = Array.from(
+//   document.querySelectorAll("#cd-comps-datalist option"),
+// );
+// map the options' text to a valid array
+// const validCdCompsLocs = cdCompOptionElems.map((el) => el.value);
+// the input element itself to access the current value
+// const cdCompsInput = document.getElementById("cd-comps-location");
+
+// ----- convert track data from a long string to nested arrays
+// the whole string
+// const tracksFull = formData.get("tracks").trim().split("\n");
+// initialize to undefined to later test easier for empty tracks field
+// let tracksToSend = undefined;
+
+// loop through and break down each track to array of artist and title
+// tracksFull.forEach((tr) => {
+// i use the pipe to split on
+// const track = tr.split("|");
+// if (!track[0] || !track[1]) {
+//   toasty("Check your track data. Must be <artist>|<title>.", "red");
+//   toasty(
+//     `${
+//       track[0] === ""
+//         ? "All tracks must have an artist"
+//         : "All tracks must have a track name"
+//     }`,
+//     "red",
+//   );
+//   return;
+// }
+// if (track.length === 2) {
+// if still undefined, create an empty array
+// if (!tracksToSend) {
+//   tracksToSend = [];
+// }
+// cleanup and push
+//     track[0] = track[0].trim();
+//     track[1] = track[1].trim();
+//     tracksToSend.push(track);
+//   } else {
+//     toasty("Check your track data. Must be <artist>|<title>.", "red");
+//     return;
+//   }
+// });
+
+// const data = {
+//   title: formData.get("title"),
+//   year: Number(formData.get("year")),
+//   location: formData.get("location"),
+//   tracks: tracksToSend,
+// };
+
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify(trimDataFields(data)),
+// };
+
+// if (!noEmptyFields(data, true)) {
+//   toasty("All fields must be filled out.", "red");
+//   return;
+// }
+
+// if (!yearFormatIsGood(data.year)) {
+//   toasty("Year must be 4 digits.", "red");
+//   return;
+// }
+
+// if (
+//   noEmptyFields(data, true) &&
+//   isLocValValid(cdCompsInput, validCdCompsLocs)
+// ) {
+//   try {
+//     const res = await fetch("/cd-comps", options);
+
+//     if (res.status === 400) {
+//       const errs = await res.json();
+//       errs.forEach((er) => {
+//         toasty(`Value: ${er.value} ; Message: ${er.msg}`, "red");
+//         console.log(`Value: ${er.value} ; Message: ${er.msg}`);
+//       });
+//       return;
+//     }
+
+//     if (res.status === 201) {
+//       const resData = await res.json();
+//       const id = resData.titleId;
+
+//       cdCompsForm.reset();
+//       toasty(
+//         `${data.title} has been added to the database with id ${id}.`,
+//         "green",
+//       );
+
+//       if (incrementLocationSwitch()) {
+//         await getLocations();
+//         handleIncrementReset();
+//       }
+
+//       if (!showSessionList) {
+//         showSessionList = true;
+//         sessionListWrapper.style.display = "block";
+//       }
+
+// add item data to the session list
+//         const sessionListStr = `id: ${id} ${data.title} was added to cd comps`;
+//         addToSessionList(sessionList, sessionListStr, "cd-comp-color");
+//         focusFirstField(cdCompsForm);
+//         console.log("new item id: ", id);
+//         window.scrollTo(0, 0);
+//       }
+//     } catch (error) {
+//       toasty(error, "red");
+//       console.log(error);
+//     }
+//   }
+// }
+
+// async function handleCdSinglesForm(e) {
+//   e.preventDefault();
+// get the form data
+// const formData = new FormData(cdSinglesForm);
+// break down the tracks string to an array, each track gets trimmed later
+// const trackList = formData.get("tracks").trim().split("\n");
+// get the option elems
+// const cdSinglesOptionElems = Array.from(
+//   document.querySelectorAll("#cd-singles-datalist option"),
+// );
+// map the options' text to a valid array
+// const validCdSingleLocs = cdSinglesOptionElems.map((el) => el.value);
+// the input element itself to access the current value
+// const cdSinglesInput = document.getElementById("cd-singles-case-type");
+
+// const data = {
+//   artist: formData.get("artist"),
+//   title: formData.get("title"),
+//   year: Number(formData.get("year")),
+//   caseType: formData.get("caseType"),
+//   tracks: trimTracks(trackList),
+// };
+
+// if (!noEmptyFields(data, true)) {
+//   toasty("All fields must be filled out.", "red");
+//   return;
+// }
+
+// if (!yearFormatIsGood(data.year)) {
+//   toasty("Year must be 4 digits", "red");
+//   return;
+// }
+
+// if only the tracks are empty
+// if (!trackList[0] && noEmptyFields(data, true)) {
+//   toasty("Please add some tracks.", "red");
+//   return;
+// }
+
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify(trimDataFields(data)),
+// };
+
+// if (
+//   noEmptyFields(data, true) &&
+//   isLocValValid(cdSinglesInput, validCdSingleLocs)
+// ) {
+//   try {
+//     const res = await fetch("/cd-singles", options);
+
+//     if (res.status === 400) {
+//       const errs = await res.json();
+//       errs.forEach((er) => {
+//         toasty(`Value: ${er.value} ; Message: ${er.msg}`, "red");
+//         console.log(`Value: ${er.value} ; Message: ${er.msg}`);
+//       });
+//       return;
+//     }
+
+//     if (res.status === 201) {
+//       const id = await res.json();
+//       cdSinglesForm.reset();
+//       focusFirstField(cdSinglesForm);
+//       toasty(
+//         `${data.artist} - ${data.title} was added to the database with id: ${id}`,
+//         "green",
+//       );
+
+//       console.log("new item id: ", id);
+
+//       if (!showSessionList) {
+//         showSessionList = true;
+//         sessionListWrapper.style.display = "block";
+//       }
+
+//       window.scrollTo(0, 0);
+
+// add item data to the session list
+// const sessionListStr = `id: ${id} ${data.artist} - ${data.title} was added to cd singles.`;
+// addToSessionList(sessionList, sessionListStr, "cd-single-color");
+// these locations are fixed and will not be incremented
+//       }
+//     } catch (error) {
+//       toasty(error, "red");
+//       console.log(error);
+//     }
+//   }
+// }
+
+// async function handleCdsMainForm(e) {
+//   e.preventDefault();
+//   const formData = new FormData(cdsMainForm);
+
+// get the option elements
+// const cdsMainOptionElems = Array.from(
+//   document.querySelectorAll("#cds-main-datalist option"),
+// );
+
+// map the options' text to a valid array
+// const validCdsMainLocs = cdsMainOptionElems.map((el) => el.value);
+
+// the input element itself to access the current value
+// const cdsMainInput = document.getElementById("cds-main-location");
+
+// const data = {
+//   artist: formData.get("artist"),
+//   title: formData.get("title"),
+//   location: formData.get("location"),
+// };
+
+// if (!noEmptyFields(data, false)) {
+//   toasty("All fields must be filled out.", "red");
+//   return;
+// }
+
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(trimDataFields(data)),
+// };
+
+// if (isLocValValid(cdsMainInput, validCdsMainLocs)) {
+//   try {
+//     const res = await fetch("/cds-main", options);
+
+//     if (res.status === 400) {
+//       const errs = await res.json();
+//       errs.forEach((er) => {
+//         toasty(`Value: ${er.value} ; Message: ${er.msg}`, "red");
+//         console.log(`Value: ${er.value} ; Message: ${er.msg}`);
+//       });
+//       return;
+//     }
+
+//     if (res.status === 201) {
+//       const resData = await res.json();
+//       toasty(
+//         `${data.artist} - ${data.title} has been added to the database with id: ${resData}`,
+//         "green",
+//       );
+
+//       cdsMainForm.reset();
+//       focusFirstField(cdsMainForm);
+//       console.log("new item id: ", resData);
+
+//       if (incrementLocationSwitch()) {
+//         await getLocations();
+//         handleIncrementReset();
+//       }
+
+//       if (!showSessionList) {
+//         showSessionList = true;
+//         sessionListWrapper.style.display = "block";
+//       }
+
+//       window.scrollTo(0, 0);
+
+// add item data to the session list
+//         const sessionListStr = `id: ${resData} ${data.artist} - ${data.title} was added to cds main.`;
+//         addToSessionList(sessionList, sessionListStr, "cds-main-color");
+//       }
+//     } catch (error) {
+//       toasty(error, "red");
+//       console.log(error);
+//     }
+//   }
+// }
+
+// async function handleRecordsForm(e) {
+//   e.preventDefault();
+//   const formData = new FormData(recordsForm);
+
+// get the option elements
+// const recordsOptionElems = Array.from(
+//   document.querySelectorAll("#records-datalist option"),
+// );
+
+// map the options' text to a valid array
+// const validRecordsLocs = recordsOptionElems.map((el) => el.value);
+
+// the input element itself to access the current value
+// const recordsInput = document.getElementById("records-location");
+
+// const data = {
+//   artist: formData.get("artist"),
+//   title: formData.get("title"),
+//   location: formData.get("location"),
+//   year: Number(formData.get("year")),
+//   diameter: formData.get("diameter"),
+//   sleeve_condition: formData.get("sleeveCondition"),
+//   record_condition: formData.get("recordCondition"),
+//   label: formData.get("label"),
+// };
+
+// if (!noEmptyFields(data, false)) {
+//   toasty("All fields must be filled out.", "red");
+//   console.log("All fields must be filled out.");
+//   return;
+// }
+
+// if (!yearFormatIsGood(data.year)) {
+//   toasty("Year must be 4 digits", "red");
+//   return;
+// }
+
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify(trimDataFields(data)),
+// };
+
+// if (
+//   noEmptyFields(data, false) &&
+//   isLocValValid(recordsInput, validRecordsLocs)
+// ) {
+//   try {
+//     const res = await fetch("/records", options);
+
+//     if (res.status === 400) {
+//       const errs = await res.json();
+//       errs.forEach((er) => {
+//         toasty(`Value: ${er.value} ; Message: ${er.msg}`, "red");
+//         console.log(`Value: ${er.value} ; Message: ${er.msg}`);
+//       });
+//       return;
+//     }
+
+//     if (res.status === 201) {
+//       const resData = await res.json();
+//       recordsForm.reset();
+//       focusFirstField(recordsForm);
+//       toasty(
+//         `${data.artist} - ${data.title} has been added to the database with id: ${resData}`,
+//         "green",
+//       );
+
+//       console.log("new item id: ", resData);
+
+//       if (incrementLocationSwitch()) {
+//         await getLocations();
+//         handleIncrementReset();
+//       }
+
+//       if (!showSessionList) {
+//         showSessionList = true;
+//         sessionListWrapper.style.display = "block";
+//       }
+
+//       window.scrollTo(0, 0);
+
+// add item data to the session list
+//         const sessionListStr = `id: ${resData} ${data.artist} - ${data.title} was added to records.`;
+//         addToSessionList(sessionList, sessionListStr, "record-color");
+//       }
+//     } catch (error) {
+//       toasty(error, "red");
+//       console.log(error);
+//     }
+//   }
+// }
+
+// async function handleTapesForm(e) {
+//   e.preventDefault();
+//   const formData = new FormData(tapesForm);
+
+// get the option elements
+// const tapesOptionElems = Array.from(
+//   document.querySelectorAll("#tapes-datalist option"),
+// );
+
+// map the options' text to a valid array
+// const validTapesLocs = tapesOptionElems.map((el) => el.value);
+
+// the input element itself to access the current value
+// const tapesInput = document.getElementById("tapes-location");
+
+// const data = {
+//   artist: formData.get("artist"),
+//   title: formData.get("title"),
+//   location: formData.get("location"),
+//   year: Number(formData.get("year")),
+//   needsRepair: formData.get("needsRepair"),
+//   speed: formData.get("tapeSpeed"),
+// };
+
+// if (!noEmptyFields(data, false)) {
+//   toasty("All fields must be filled out.", "red");
+//   console.log("All fields must be filled out.");
+//   return;
+// }
+
+// if (!yearFormatIsGood(data.year)) {
+//   toasty("Year must be 4 digits", "red");
+//   return;
+// }
+
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify(trimDataFields(data)),
+// };
+
+// if (noEmptyFields(data, false) && isLocValValid(tapesInput, validTapesLocs)) {
+//   try {
+//     const res = await fetch("/tapes", options);
+
+//     if (res.status === 400) {
+//       const errs = await res.json();
+//       errs.forEach((er) => {
+//         toasty(`Value: ${er.value} ; Message: ${er.msg}`, "red");
+//         console.log(`Value: ${er.value} ; Message: ${er.msg}`);
+//       });
+//       return;
+//     }
+
+//     if (res.status === 201) {
+//       const resData = await res.json();
+
+//       tapesForm.reset();
+//       focusFirstField(tapesForm);
+//       toasty(
+//         `${data.artist} - ${data.title} has been added to the database with id: ${resData}`,
+//         "green",
+//       );
+
+//       if (incrementLocationSwitch()) {
+//         await getLocations();
+//         handleIncrementReset();
+//       }
+
+//       console.log("new item id: ", resData);
+
+//       if (!showSessionList) {
+//         showSessionList = true;
+//         sessionListWrapper.style.display = "block";
+//       }
+
+//       window.scrollTo(0, 0);
+
+// add item data to the session list
+//         const sessionListStr = `id: ${resData} ${data.artist} - ${data.title} was added to tapes.`;
+//         addToSessionList(sessionList, sessionListStr, "tape-color");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+
+// add the listeners to the nav btns
+// navButtons.forEach((btn) => {
+//   btn.addEventListener("click", handleNavBtnClick);
+// });
+
+// if the increment checkbox is checked and then the location is changed
+// const selects = document.querySelectorAll("select");
+// selects.forEach((sel) => {
+//   sel.addEventListener("change", handleIncrementReset);
+// });
+
+// submit listeners on the forms
+// cdsMainForm.addEventListener("submit", handleCdsMainForm);
+// cdCompsForm.addEventListener("submit", handleCdCompsForm);
+// cdSinglesForm.addEventListener("submit", handleCdSinglesForm);
+// recordsForm.addEventListener("submit", handleRecordsForm);
+// tapesForm.addEventListener("submit", handleTapesForm);
+// incrementCheckbox.addEventListener("change", () => {
+//   handleCheckbox(forms);
+// });
+
+// slider
+const themeSlider = document.getElementById("theme-slider");
+const slide = document.getElementById("slide");
+let theme = document.documentElement.getAttribute("data-theme");
+
+function handleThemeToggle() {
+  if (theme === "dark") {
+    theme = "light";
+    slide.style.right = ".125rem";
+    handleThemeChange();
+  } else {
+    theme = "dark";
+    slide.style.right = "1.125rem";
+    handleThemeChange();
+  }
+}
+
+themeSlider.addEventListener("click", handleThemeToggle);
---
+++ add-utils.js
@@ -0,0 +1,179 @@
+// import Toastify from "./node_modules/toastify-js/src/toastify-es.js";
+
+// reset all forms to not active
+// export function removeActiveFormClass(forms) {
+//   forms.forEach((form) => {
+//     form.classList.remove("active-form");
+//   });
+// }
+
+// frontend validation for the year field, did this way to use toastify instead of user-agent message
+// export function yearFormatIsGood(year) {
+//   const regex = /^[0-9]{4}$/;
+//   return regex.test(year);
+// }
+
+// check the data objects for empty vals
+// export function noEmptyFields(data, tracksTrigger) {
+//   if (tracksTrigger && !data.tracks) {
+//     return false;
+//   }
+
+//   if ("year" in data && Number.isNaN(data.year)) {
+//     toasty("Year must be a number.", "red");
+//     return;
+//   }
+
+//   for (const key in data) {
+//     if (!data[key]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// our toast config function
+// export function toasty(msg, color) {
+//   let bg;
+
+//   if (color === "red") {
+//     bg = "rgba(172, 38, 38, 1)";
+//   }
+//   if (color === "green") {
+//     bg = "rgba(9, 119, 9, 1)";
+//   }
+//   if (!bg) {
+//     bg = color;
+//   }
+
+//   Toastify({
+//     text: msg,
+//     duration: 5000,
+//     gravity: "top",
+//     position: "center",
+//     style: {
+//       background: bg,
+//       fontSize: "1.5rem",
+//       width: "fit-content",
+//       maxWidth: "640px",
+//       textAlign: "center",
+//       borderRadius: "10px",
+//     },
+//   }).showToast();
+// }
+
+// trim singles track titles
+// export function trimTracks(arr) {
+//   const trimmed = [];
+//   arr.forEach((tr) => trimmed.push(tr.trim()));
+//   return trimmed;
+// }
+
+// export function handleIncrementLocation(form) {
+//   // now we take this form and change the select options value and text content
+//   const selectedOption = Array.from(form.querySelectorAll("option")).filter(
+//     (opt) => opt.selected,
+//   )[0];
+//   // take option value and split
+//   const splitOptionVal = selectedOption.value.split(" ");
+
+//   // take the -1 index and parse as num
+//   let numVal = parseInt(splitOptionVal.at(-1));
+//   // if one of the options without an ending number is selected, return
+//   if (Number.isNaN(numVal)) return;
+//   // increment num and add back to string
+//   const incrementedNumString = (numVal += 1).toString();
+//   splitOptionVal[splitOptionVal.length - 1] = incrementedNumString;
+//   const reassembledString = splitOptionVal.join(" ");
+
+//   // set the new vals
+
+//   selectedOption.value = reassembledString;
+//   selectedOption.textContent = reassembledString;
+// }
+
+// export const incrementCheckbox = document.getElementById("increment-location");
+
+// export function handleIncrementReset() {
+//   incrementCheckbox.checked = false;
+// }
+
+// export const incrementLocationSwitch = () => incrementCheckbox.checked;
+
+// when the incr box is checked, get the active form
+// export function handleCheckbox(arr) {
+//   const activeForm = arr.filter((form) =>
+//     form.classList.contains("active-form"),
+//   );
+//   if (incrementLocationSwitch()) {
+//     if (
+//       window.confirm(
+//         "Once incremented, to undo, for now just refresh the page.",
+//       )
+//     ) {
+//       handleIncrementLocation(activeForm[0]);
+//     } else {
+//       handleIncrementReset();
+//       return;
+//     }
+//   }
+// }
+
+// export function trimDataFields(data) {
+//   for (const key in data) {
+//     if (typeof data[key] === "string") {
+//       data[key] = data[key].trim();
+//     }
+//   }
+//   return data;
+// }
+
+// export function addToSessionList(list, str, className) {
+//   const li = document.createElement("li");
+//   li.textContent = str;
+//   li.classList.add(className);
+//   list.prepend(li);
+// }
+
+// export function focusFirstField(form) {
+//   const firstField = form.querySelector("input");
+//   firstField.focus();
+// }
+
+export function handleThemeChange() {
+  const currTheme = document.documentElement.getAttribute("data-theme");
+  document.documentElement.style.colorScheme =
+    currTheme === "light" ? "dark" : "light";
+  document.documentElement.setAttribute(
+    "data-theme",
+    currTheme === "light" ? "dark" : "light",
+  );
+}
+
+// export function removeActiveClass(btns) {
+//   btns.forEach((btn) => {
+//     btn.classList.remove("active-nav-btn");
+//   });
+// }
+
+// export function showForm(formStr, navBtn) {
+//   navBtn.classList.add("active-nav-btn");
+//   const activeForm = document.getElementById(formStr);
+//   activeForm.classList.add("active-form");
+//   activeForm.querySelector("input").focus();
+// }
+
+// export function initialShowForm(mainEl, incrementWrapper) {
+//   incrementWrapper.style.display = "block";
+//   mainEl.style.opacity = 1;
+// }
+
+// export function isLocValValid(inputEl, validArr) {
+//   if (validArr.includes(inputEl.value)) {
+//     return true;
+//   } else {
+//     toasty("Location field does not contain a valid value.", "red");
+//     return false;
+//   }
+// }
