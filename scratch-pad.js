cd50f4d20ffed4e6acedccc084a6d02bb76d4c19
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-04-19 06:41:48 -0400

    scratch pad now functions and retains value when switching pages

--- scratch-pad.js
+++ scratch-pad.js
@@ -1,3 +1,4 @@
+import { addToSessionStore } from "./add-utils.js";
 let drawerIsOpen = false;
 const scratchPad = document.querySelector(".scratch-pad");
 const scratchPadSpan = document.querySelector(".scratch-pad span");
@@ -7,13 +8,27 @@ scratchPadSpan.addEventListener("click", (e) => {
   drawerIsOpen = !drawerIsOpen;
   if (drawerIsOpen) {
     scratchPad.style.left = "255px";
+    scratchPadSpan.style.rotate = "180deg";
   } else {
     scratchPad.style.left = "0px";
+    scratchPadSpan.style.rotate = "0deg";
   }
 });
 
 // get the value of scratch pad into session store
 textArea.addEventListener("input", (e) => {
-  console.log(e.target.value);
+  addToSessionStore("", e.target.value, "scratchPad");
 });
 // populate the text area with the session store value on page load
+async function loadScratchPadVal() {
+  const scratchPadCurrVal = await sessionStore.sessionGet(
+    "sessionGet",
+    "scratchPad",
+  );
+
+  if (scratchPadCurrVal) {
+    textArea.value = scratchPadCurrVal;
+  }
+}
+
+loadScratchPadVal();
