0476d86f38831773a1f4aa945f7c82749e35399e
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-06-24 19:16:47 -0400

    refactored and added more tests for the homepage

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,27 +1,75 @@
 import { test, expect, _electron as electron } from "@playwright/test";
 
 test.describe("HOMEPAGE", () => {
-  test("Application launches and loads UI", async () => {
+  let electronApp;
+  let page;
+
+  test.beforeAll(async () => {
     // Launch the Electron application pointing to your main entry file (e.g., main.js)
-    const electronApp = await electron.launch({ args: ["./main.js"] });
-
+    electronApp = await electron.launch({ args: ["./main.js"] });
     // Wait for the first BrowserWindow to open
-    const page = await electronApp.firstWindow();
+    page = await electronApp.firstWindow();
+    console.log(page.url());
+  });
 
+  test.afterAll(async () => {
+    // Close the app at the end of the test
+    await electronApp.close();
+  });
+
+  test.beforeEach(async () => {
+    const currentUrl = page.url();
+    const isHomepage = currentUrl.endsWith("index.html");
+    const dialog = page.getByRole("dialog");
+
+    if (await dialog.isVisible()) {
+      await page.getByRole("button", { name: "x" }).click();
+      await expect(dialog).not.toBeVisible();
+    }
+
+    if (!isHomepage) {
+      await page.getByAltText("the majewski collection").click();
+    }
+  });
+
+  test("Application launches and loads UI", async () => {
     const button = page.getByRole("button", { name: "UPDATE WEB CATALOG" });
     const lookupLink = page.getByRole("link", { name: "LOOKUP" });
     const addLink = page.getByRole("link", { name: "ADD ITEMS" });
     const updateLink = page.getByRole("link", { name: "UPDATE ITEMS" });
-
     await expect(page).toHaveTitle("The Majewski Collection App");
     await expect(button).toBeVisible();
     await expect(lookupLink).toBeVisible();
     await expect(addLink).toBeVisible();
     await expect(updateLink).toBeVisible();
+  });
 
+  test("Clicking the LOOKUP button goes to the Lookup page", async () => {
+    const lookupLink = page.getByRole("link", { name: "LOOKUP" });
     await lookupLink.click();
     await expect(page).toHaveTitle("The Majewski Collection Lookup");
-    // Close the app at the end of the test
-    await electronApp.close();
   });
+
+  test("Clicking the ADD ITEMS button goes to the insert page", async () => {
+    const addLink = page.getByRole("link", { name: "ADD ITEMS" });
+    await addLink.click();
+    await expect(page).toHaveTitle("The Majewski Collection Add Items");
+  });
+
+  test("Clicking the UPDATE ITEMS button goes to the update page", async () => {
+    const updateLink = page.getByRole("link", { name: "UPDATE ITEMS" });
+    await updateLink.click();
+    await expect(page).toHaveTitle("The Majewski Collection Update Items");
+  });
+
+  test("Clicking the UPDATE WEB CATALOG button opens the confirmation dialog", async () => {
+    const button = page.getByRole("button", { name: "UPDATE WEB CATALOG" });
+    const dialog = await page.getByRole("dialog");
+
+    await button.click();
+
+    await expect(dialog).toBeVisible();
+  });
+
+  test.skip("test the output of the update web catalog button", async () => {});
 });
