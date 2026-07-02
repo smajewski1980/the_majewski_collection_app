0d23f89a101b4221a956b880da495d8babd6bc92
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-07-02 10:34:27 -0400

    wrote tests that the add page nav buttons load the correct forms in add-items.spec.js

--- tests/add-items.spec.js
+++ tests/add-items.spec.js
@@ -5,6 +5,12 @@ import { test, expect, _electron as electron } from "@playwright/test";
 let electronApp;
 let page;
 
+const CD_COMPS_FORM_ID = "cd-comps-form";
+const CD_SINGLES_FORM_ID = "cd-singles-form";
+const CD_MAIN_FORM_ID = "cd-main-form";
+const RECORDS_FORM_ID = "records-form";
+const TAPES_FORM_ID = "tapes-form";
+
 test.beforeAll(async () => {
   // Launch the Electron application pointing to your main entry file (e.g., main.js)
   electronApp = await electron.launch({
@@ -31,16 +37,55 @@ test.afterAll(async () => {
 });
 
 test.describe("ADD ITEMS", () => {
-  test("blah blah", () => {
-    expect(true).toBe(true);
-  });
-
   test.describe("NAV buttons render the proper forms", () => {
-    test.skip("When the Cd-Compilations button is clicked, the Cd-Compilations form is displayed.", async () => {});
-    test.skip("When the Cd-Singles button is clicked, the Cd-Singles form is displayed.", async () => {});
-    test.skip("When the Cd-Main Catalog button is clicked, the Cd-Main Catalog form is displayed.", async () => {});
-    test.skip("When the Records button is clicked, the Records form is displayed.", async () => {});
-    test.skip("When the Tapes button is clicked, the Tapes form is displayed.", async () => {});
+    test.beforeEach(async () => {
+      await page.reload();
+    });
+
+    test("When the Cd-Compilations button is clicked, the Cd-Compilations form is displayed.", async () => {
+      const compsNavBtn = page.getByRole("button", { name: "Cd-Compilations" });
+      await compsNavBtn.click();
+
+      const activeForm = await page.locator(".active-form");
+
+      await expect(activeForm).toHaveId(CD_COMPS_FORM_ID);
+    });
+
+    test("When the Cd-Singles button is clicked, the Cd-Singles form is displayed.", async () => {
+      const singlesNavBtn = page.getByRole("button", { name: "Cd-Singles" });
+      await singlesNavBtn.click();
+
+      const activeForm = await page.locator(".active-form");
+
+      await expect(activeForm).toHaveId(CD_SINGLES_FORM_ID);
+    });
+
+    test("When the Cd-Main Catalog button is clicked, the Cd-Main Catalog form is displayed.", async () => {
+      const cdsNavBtn = page.getByRole("button", { name: "Cd-Main Catalog" });
+      await cdsNavBtn.click();
+
+      const activeForm = await page.locator(".active-form");
+
+      await expect(activeForm).toHaveId(CD_MAIN_FORM_ID);
+    });
+
+    test("When the Records button is clicked, the Records form is displayed.", async () => {
+      const recordsNavBtn = page.getByRole("button", { name: "Records" });
+      await recordsNavBtn.click();
+
+      const activeForm = await page.locator(".active-form");
+
+      await expect(activeForm).toHaveId(RECORDS_FORM_ID);
+    });
+
+    test("When the Tapes button is clicked, the Tapes form is displayed.", async () => {
+      const tapesNavBtn = page.getByRole("button", { name: "Tapes" });
+      await tapesNavBtn.click();
+
+      const activeForm = await page.locator(".active-form");
+
+      await expect(activeForm).toHaveId(TAPES_FORM_ID);
+    });
   });
 
   test.describe("The forms", () => {
