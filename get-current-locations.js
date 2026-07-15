dd0e224719633fc510daae6fd5e73478cdd38e11
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-07-15 19:52:38 -0400

    refactored get-current-location.js and handleGetCurrentLocation.js

--- get-current-locations.js
+++ get-current-locations.js
@@ -1,19 +1,6 @@
 import { toasty, getFormClassStr } from "./add-utils.js";
 
 /**
- * sort numerically instead of lexicographically
- * @param {string} a
- * @param {string} b
- * @returns {number}
- */
-function customSort(a, b) {
-  return a.localeCompare(b, undefined, {
-    numeric: true,
-    sensitivity: "base",
-  });
-}
-
-/**
  * filters a datalist to only have options that start with a given value
  * @param {HTMLDataListElement} datalist - the datalist to work on
  * @param {string} value - the string to filter on
@@ -195,302 +182,6 @@ function addCustomDatalistListeners(input, datalist) {
   });
 }
 
-/**
- * sort the given cd main catalog locations and return currents
- * @param {Object[]} locations array of location objects to be sorted
- * @param {string} locations[].location - location string
- * @returns {string[]} returns an array of current locations
- */
-function getMostCurrentCdsMainLoc(locations) {
-  const misc = [];
-  const classical = [];
-  const country = [];
-  const hipHop = [];
-  const jazz = [];
-  const nature = [];
-  const pop = [];
-  const world = [];
-  const xmas = [];
-
-  // sort out by category
-  locations.forEach((loc) => {
-    if (loc.location.includes("Classical")) {
-      classical.push(loc.location);
-    } else if (loc.location.includes("Country")) {
-      country.push(loc.location);
-    } else if (loc.location.includes("Hip Hop")) {
-      hipHop.push(loc.location);
-    } else if (loc.location.includes("Jazz")) {
-      jazz.push(loc.location);
-    } else if (loc.location.includes("Nature")) {
-      nature.push(loc.location);
-    } else if (loc.location.includes("Pop/Rock")) {
-      pop.push(loc.location);
-    } else if (loc.location.includes("World")) {
-      world.push(loc.location);
-    } else if (loc.location.includes("X-mas")) {
-      xmas.push(loc.location);
-    } else {
-      misc.push(loc.location);
-    }
-  });
-
-  // get current sorted vals
-  const currClassical = classical.sort(customSort);
-  const currCountry = country.sort(customSort);
-  const currHipHop = hipHop.sort(customSort);
-  const currJazz = jazz.sort(customSort);
-  const currNature = nature.sort(customSort);
-  const currPop = pop.sort(customSort);
-  const currWorld = world.sort(customSort);
-  const currXmas = xmas.sort(customSort);
-
-  // return vals
-  return [
-    currClassical.at(-1),
-    currCountry.at(-1),
-    currHipHop.at(-1),
-    currJazz.at(-1),
-    currNature.at(-1),
-    currPop.at(-1),
-    currWorld.at(-1),
-    currXmas.at(-1),
-    ...misc,
-  ];
-}
-
-/**
- * sort the given cd comps locations and return currents
- * @param {Object[]} locations the locations to be sorted
- * @param {string} locations[].location - location string
- * @returns {string[]} returns an array of current locations
- */
-function getMostCurrentCdComps(locations) {
-  // sort out by category
-  const classical = [];
-  const soundtracks = [];
-  const va = [];
-  const xmas = [];
-  const misc = [];
-
-  locations.forEach((loc) => {
-    if (loc.location.includes("Classical")) {
-      classical.push(loc.location);
-    }
-    if (loc.location.includes("Soundtrack")) {
-      soundtracks.push(loc.location);
-    }
-    if (loc.location.includes("Various")) {
-      va.push(loc.location);
-    }
-    if (loc.location.includes("X-mas")) {
-      xmas.push(loc.location);
-    }
-    if (loc.location.includes("Good Location")) {
-      misc.push(loc.location);
-    }
-  });
-
-  // get current sorted vals
-  const currClassical = classical.sort(customSort);
-  const currSoundtracks = soundtracks.sort(customSort);
-  const currVarious = va.sort(customSort);
-  const currXmas = xmas.sort(customSort);
-
-  return [
-    currClassical.at(-1),
-    currSoundtracks.at(-1),
-    currVarious.at(-1),
-    currXmas.at(-1),
-    ...misc,
-  ];
-}
-
-/**
- * format the cd singles locations to be consistent with the others
- * @param {Object[]} singlesLocs [{case_type: '<case_type>'}]
- * @param {string} singlesLocs[].case_type - the location/case type
- * @returns {string[]} returns an array of cd singles locations
- */
-function formatCdSinglesLocs(singlesLocs) {
-  const singleLocsArr = [];
-
-  singlesLocs.forEach((loc) => {
-    singleLocsArr.push(loc.case_type);
-  });
-
-  return singleLocsArr;
-}
-
-/**
- * sort the given 33s locations and return currents
- * @param {string[]} loc33s the locations to be sorted
- * @returns {string[]} returns an array of current locations
- */
-function getMostCurrent33sLoc(loc33s) {
-  const misc = [];
-  const classical = [];
-  const comedy = [];
-  const country = [];
-  const jazz = [];
-  const unopened = [];
-  const pop = [];
-  const soundtracks = [];
-  const various = [];
-
-  // sort out by category
-  loc33s.forEach((loc) => {
-    const currLoc = loc.split(" ")[1];
-    if (currLoc === "Classical") {
-      classical.push(loc);
-    } else if (currLoc === "Country") {
-      country.push(loc);
-    } else if (currLoc === "Comedy") {
-      comedy.push(loc);
-    } else if (currLoc === "Jazz") {
-      jazz.push(loc);
-    } else if (currLoc === "NEW") {
-      unopened.push(loc);
-    } else if (currLoc === "Pop/Rock") {
-      pop.push(loc);
-    } else if (currLoc === "Soundtracks") {
-      soundtracks.push(loc);
-    } else if (currLoc === "Various") {
-      various.push(loc);
-    } else {
-      misc.push(loc);
-    }
-  });
-
-  // get current vals
-  const currClassical = classical.sort(customSort);
-  const currCountry = country.sort(customSort);
-  const currComedy = comedy.sort(customSort);
-  const currJazz = jazz.sort(customSort);
-  const currUnopened = unopened.sort(customSort);
-  const currPop = pop.sort(customSort);
-  const currSoundtracks = soundtracks.sort(customSort);
-  const currVarious = various.sort(customSort);
-
-  // return array with the vals
-  return [
-    currClassical.at(-1),
-    currCountry.at(-1),
-    currComedy.at(-1),
-    currJazz.at(-1),
-    currUnopened.at(-1),
-    currPop.at(-1),
-    currSoundtracks.at(-1),
-    currVarious.at(-1),
-    ...misc,
-  ];
-}
-
-/**
- * sort the given records locations and return currents
- * @param {Object[]} locations the locations to be sorted
- * @param {string} locations[].location - the record location string
- * @returns {string[]} returns an array of current locations
- */
-function getMostCurrentRecordsLoc(locations) {
-  const rec45s = [];
-  const rec33s = [];
-  const rec78s = [];
-  // when we switch over to this app, we will change the name of the herb loc to start with 33s in the db
-  const misc = ["Herb Alpert Records", '12" Singles'];
-
-  // distibute the values to the appropriate array
-  locations.forEach((loc) => {
-    if (loc.location.includes("45s ")) {
-      rec45s.push(loc.location);
-    }
-    if (loc.location.includes("33s ")) {
-      rec33s.push(loc.location);
-    }
-    if (loc.location.includes("78s ")) {
-      rec78s.push(loc.location);
-    }
-    if (loc.location.includes("Good Location")) {
-      misc.push(loc.location);
-    }
-  });
-
-  // sort the sortables and get currents
-  const sorted45s = rec45s.sort(customSort);
-  const sorted78s = rec78s.sort(customSort);
-
-  const curren33sLocs = getMostCurrent33sLoc(rec33s);
-  const current45sLoc = sorted45s.at(-1);
-  const current78sLoc = sorted78s.at(-1);
-
-  return [...curren33sLocs, current45sLoc, current78sLoc, ...misc];
-}
-
-/**
- * sort the given tapes locations and return currents
- * @param {Object[]} locations the locations to be sorted
- * @param {string} locations[].location - the tapes location string
- * @returns {string[]} returns an array of current locations
- */
-function getMostCurrentTapeLoc(locations) {
-  const eightTracks = [];
-  const reelToReel = [];
-  const cassettes = [];
-  const misc = [];
-
-  // distribute the values to the appropriate array
-  locations.forEach((loc) => {
-    if (loc.location.includes("8-Track")) {
-      eightTracks.push(loc.location);
-    }
-    if (loc.location.includes("Reel")) {
-      reelToReel.push(loc.location);
-    }
-    if (loc.location.includes("Cassette")) {
-      cassettes.push(loc.location);
-    }
-    if (loc.location.includes("Good Location")) {
-      misc.push(loc.location);
-    }
-  });
-
-  // sort the arrays
-  const sortedEightTracks = eightTracks.sort(customSort);
-  const sortedReelToReel = reelToReel.sort(customSort);
-  const sortedCassettes = cassettes.sort(customSort);
-
-  // set the highest value as current
-  const currEightLoc = sortedEightTracks.at(-1);
-  const currReelLoc = sortedReelToReel.at(-1);
-  const currCassLoc = sortedCassettes.at(-1);
-
-  return [currEightLoc, currCassLoc, currReelLoc, ...misc];
-}
-
-/**
- * takes locations and a format and feeds them to the appropriate function
- * @param {Object[]} locations the locations to be sorted
- * @param {string} locations[].location - location string
- * @param {string} format - format string
- * @returns {string[]} returns an array of current locations
- */
-function getMostCurrentLoc(locations, format) {
-  switch (format) {
-    case "tapes":
-      return getMostCurrentTapeLoc(locations);
-    case "records":
-      return getMostCurrentRecordsLoc(locations);
-    case "cds":
-      return getMostCurrentCdsMainLoc(locations);
-    case "cdSingles":
-      return formatCdSinglesLocs(locations);
-    case "cdComps":
-      return getMostCurrentCdComps(locations);
-    default:
-      return [];
-  }
-}
-
 // the HTML datalist elements
 const cdCompsSelect = document.getElementById("cd-comps-datalist");
 const cdsMainSelect = document.getElementById("cds-main-datalist");
@@ -562,22 +253,22 @@ let currCdsMain;
  * @param {LocationData} locationData - The location data for all formats.
  */
 function processLocations(locationData) {
-  // sort the locationData
-  const tapesLocs = locationData.tapes;
-  const recordsLocs = locationData.records;
-  const cdsLocs = locationData.cds;
-  const cdCompsLocs = locationData.cdComps;
-  const cdSinglesLocs = locationData.cdSingles;
+  const {
+    highLocCdComps,
+    highLocCdSingles,
+    highLocCds,
+    highLocRecords,
+    highLocTapes,
+  } = locationData;
 
-  // get the most current locations and set variable vals
-  currTapeLoc = getMostCurrentLoc(tapesLocs, "tapes");
-  currRecordsLocs = getMostCurrentLoc(recordsLocs, "records");
-  currCdSinglesLocs = getMostCurrentLoc(cdSinglesLocs, "cdSingles");
-  currCdComps = getMostCurrentLoc(cdCompsLocs, "cdComps");
-  currCdsMain = getMostCurrentLoc(cdsLocs, "cds");
+  currCdComps = highLocCdComps;
+  currCdSinglesLocs = highLocCdSingles;
+  currCdsMain = highLocCds;
+  currRecordsLocs = highLocRecords;
+  currTapeLoc = highLocTapes;
 }
 
-// fetch the data
+// get the data
 /**
  * this is the main function that does the initial
  * fetch of the location data and feeds it to the processLocations function
@@ -589,6 +280,7 @@ export async function getLocations() {
       "getCurrentLocations",
     );
     const data = JSON.parse(await res);
+
     processLocations(data);
   } catch (error) {
     console.log(error);
--- ipc-handlers/handleGetCurrentLocations.js
+++ ipc-handlers/handleGetCurrentLocations.js
@@ -2,30 +2,65 @@ const pool = require("../dbconnect.js");
 
 async function handleGetCurrentLocations(e) {
   try {
-    const tapesResult = await pool.query(
-      "SELECT * FROM current_tapes_locations",
-    );
-    const recordsResult = await pool.query(
-      "SELECT * FROM current_records_locations",
-    );
-    const cdsResult = await pool.query("SELECT * FROM current_cds_locations");
-    const cdSinglesResult = await pool.query(
-      "SELECT * FROM current_cd_singles_locations",
-    );
-    const cdCompsResult = await pool.query(
-      "SELECT * FROM current_cd_comps_locations",
-    );
+    // const tapesResult = await pool.query(
+    //   "SELECT * FROM current_tapes_locations",
+    // );
+    // const recordsResult = await pool.query(
+    //   "SELECT * FROM current_records_locations",
+    // );
+    // const cdsResult = await pool.query("SELECT * FROM current_cds_locations");
+    // const cdSinglesResult = await pool.query(
+    //   "SELECT * FROM current_cd_singles_locations",
+    // );
+    // const cdCompsResult = await pool.query(
+    //   "SELECT * FROM current_cd_comps_locations",
+    // );
 
-    const locationData = {
-      tapes: tapesResult.rows,
-      records: recordsResult.rows,
-      cds: cdsResult.rows,
-      cdComps: cdCompsResult.rows,
-      cdSingles: cdSinglesResult.rows,
+    // const locationData = {
+    //   tapes: tapesResult.rows,
+    //   records: recordsResult.rows,
+    //   cds: cdsResult.rows,
+    //   cdComps: cdCompsResult.rows,
+    //   cdSingles: cdSinglesResult.rows,
+    // };
+
+    const [
+      highLocCdCompsRaw,
+      highLocCdSinglesRaw,
+      highLocCdsRaw,
+      highLocRecordsRaw,
+      highLocTapesRaw,
+    ] = await Promise.all([
+      pool.query("SELECT * FROM curr_high_locs_cd_comps"),
+      pool.query("SELECT * FROM curr_high_locs_cd_singles"),
+      pool.query("SELECT * FROM curr_high_locs_cds"),
+      pool.query("SELECT * FROM curr_high_locs_records"),
+      pool.query("SELECT * FROM curr_high_locs_tapes"),
+    ]);
+
+    const highLocCdComps = highLocCdCompsRaw.rows.map(
+      (r) => r.highest_location,
+    );
+    const highLocCdSingles = highLocCdSinglesRaw.rows.map(
+      (r) => r.highest_location,
+    );
+    const highLocCds = highLocCdsRaw.rows.map((r) => r.highest_location);
+    const highLocRecords = highLocRecordsRaw.rows.map(
+      (r) => r.highest_location,
+    );
+    const highLocTapes = highLocTapesRaw.rows.map((r) => r.highest_location);
+
+    const highLocData = {
+      highLocCdComps,
+      highLocCdSingles,
+      highLocCds,
+      highLocRecords,
+      highLocTapes,
     };
-    return JSON.stringify(locationData);
+
+    return JSON.stringify(highLocData);
   } catch (error) {
-    next(new Error(error));
+    console.log(error);
   }
 }
 
